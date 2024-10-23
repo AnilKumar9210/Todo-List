211547a7748eba3a638e949ea1656f4aa37e096f
Anil Kumar <avulanil17@gmail.com>
2024-10-23 12:40:00 +0530

    completed

--- Todo-list/src/App.jsx
+++ Todo-list/src/App.jsx
@@ -1,34 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , useRef} from 'react'
 import './App.css'
+import Navbar from './Components/Navbar'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo,setTodo] = useState ("")
+  const [todoList,setTodoList] = useState ([])
+  const finishedRef = useRef ("none")
+  const unFinishedRef = useRef ("block")
+  const [finishedList,setFinishedList] = useState ([])
 
+  
+  const handleChange = (e)=> {
+    setTodo (e.target.value)
+  }
+
+  const handleSave = ()=> {
+    setTodoList ([...todoList,{id:uuidv4(),todo,isCompleted:false}])
+    setTodo (" ")
+  }
+
+  const handleEdit = (id)=> {
+    let t = todoList.filter ((i)=>i.id === id)
+    setTodo (t[0].todo)
+    let newTodos = todoList.filter ((item)=> {
+      return item.id !== id
+    })
+    setTodoList (newTodos)
+  }
+  const handleDelete = (id)=> {
+    let newTodos = todoList.filter (item=> {
+      return item.id !== id;
+    })
+    setTodoList (newTodos)
+  }
+
+  const handleCompleted = (id)=> {
+    let t = todoList.filter (i=>i.id === id);
+    if (!t[0].isCompleted) {
+      let newTodos = todoList.filter (item=> {
+        return item.id !== id;
+      })
+      t[0].isCompleted= true;
+      setFinishedList (t);
+      setTodoList (newTodos)
+    }
+  }
+
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   <>
+    <Navbar/>
+    <section className="todoArea">
+      <div className="create">
+     <h1>Add Your Todo's Here</h1>
+        <div className="addTodo">
+          <input className='typeHere' onChange={handleChange}  type="text" value={todo}  />
+          <button className='btn' onClick={handleSave}>Save</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="AllTodos">
+        <h1>Your Todos</h1>
+        {todoList.map ((item)=> {
+          return<div key={item.id} className="todoList" id={unFinishedRef.current}>
+        <div className="check">
+          <input type="checkbox" onChange={()=>{handleCompleted (item.id)}} />
+          <div className="todo">{item.todo}</div>
+        </div>
+        <div className="mutate">
+          <button className="btn"onClick={(e)=>{handleEdit(item.id)}}>Edit</button>
+          <button className="btn" onClick={(e)=>{handleDelete(item.id)}}>Delete</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      })}
+      
+      </div>
+    </section>
+   </>
   )
 }
 
---
+++ Todo-list/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Completed</li>
+        <li>inComplete</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
